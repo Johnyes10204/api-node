4b2c6b73418b1405fa0d34a6c1619806c84c9176
John Nelson Florez <john.florez@aossas.com>
2024-06-12 19:14:31 -0500

    I don't know what I do

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const cors = require('cors'); // Importa el paquete cors
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
